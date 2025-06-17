7ce05931dba61b138cc71c99fa5148e2d83461f3
André L <contaproplurallanglo775@gmail.com>
2025-06-17 19:54:39 -0300

    adicionando segundo farol

--- main.js
+++ main.js
@@ -91,13 +91,17 @@ const CAM = {
 let gPausado = false;
 let caminhao;
 
+
+////!!!!!!!!!! falta atenuação do spot light
 var farol_caminhao={
-    pos: vec4(8,-0.5,0,1), // posição da frente do caminhão
+    pos1: vec4(8,-0.5,-0.65,1), // posição da frente do caminhão
     spotlightDirection :vec3(-1,0,0), // Direção do spot light em World Space (deveria ser ajustada conforme o modelo do caminhão)
     CorDifusaoSpotLoc : vec4(2, 2, 0, 1.0), // Cor Difusa do spot light
     CorEspecularSpotLoc : vec4(2, 2, 0, 1.0), // Cor Especular do spot light
     innerAngleDegrees : 0.5, // Ângulo interno do spot light
-    outerAngleDegrees : 2, // Ângulo externo do spot light
+    outerAngleDegrees : 1.8, // Ângulo externo do spot light
+
+    pos2: vec4(8,-0.5,0.65,1),
 }
 
 function main() {
@@ -177,17 +181,22 @@ function atualiza_farol_caminhao(delta) {
 
 
     // Posição global do farol
-    let pos_global_farol = add(mult(R, farol_caminhao.pos), vec4(caminhao.posicao[0], caminhao.posicao[1], caminhao.posicao[2], 1));
+    let pos_global_farol1 = add(mult(R, farol_caminhao.pos1), vec4(caminhao.posicao[0], caminhao.posicao[1], caminhao.posicao[2], 1));
+    let pos_global_farol2 = add(mult(R, farol_caminhao.pos2), vec4(caminhao.posicao[0], caminhao.posicao[1], caminhao.posicao[2], 1));
+
 
     // Atualiza a posição do farol no objeto global
 
-    gl.uniform4fv(gShader.uSpotLightPos, [pos_global_farol[0], pos_global_farol[1], pos_global_farol[2], 1.0])
+    gl.uniform4fv(gShader.uSpotLightPos1, [pos_global_farol1[0], pos_global_farol1[1], pos_global_farol1[2], 1.0])
+    gl.uniform4fv(gShader.uSpotLightPos2, [pos_global_farol2[0], pos_global_farol2[1], pos_global_farol2[2], 1.0])
+
     
 }
 
 function init_farol_caminhao() {
 
-    gl.uniform3fv(gShader.uSpotLightDirectionWorld, farol_caminhao.spotlightDirection);
+    gl.uniform3fv(gShader.uSpotLightDirectionWorld1, farol_caminhao.spotlightDirection);
+    gl.uniform3fv(gShader.uSpotLightDirectionWorld2, farol_caminhao.spotlightDirection);
     gl.uniform4fv(gShader.uCorDifusaoSpot, farol_caminhao.CorDifusaoSpotLoc);
     gl.uniform4fv(gShader.uCorEspecularSpot, farol_caminhao.CorEspecularSpotLoc);
     gl.uniform1f(gShader.uInnerCutoff, Math.cos(radians(farol_caminhao.innerAngleDegrees)));
@@ -333,8 +342,10 @@ function crieShaders() {
 
     // Obtem localizações dos uniforms da spot light
     // Uniforms do VERTEX SHADER
-    gShader.uSpotLightPos = gl.getUniformLocation(gShader.program, "uSpotLightPos");
-    gShader.uSpotLightDirectionWorld = gl.getUniformLocation(gShader.program, "uSpotLightDirectionWorld");
+    gShader.uSpotLightPos1 = gl.getUniformLocation(gShader.program, "uSpotLightPos1");
+    gShader.uSpotLightDirectionWorld1 = gl.getUniformLocation(gShader.program, "uSpotLightDirectionWorld1");
+    gShader.uSpotLightPos2 = gl.getUniformLocation(gShader.program, "uSpotLightPos2");
+    gShader.uSpotLightDirectionWorld2 = gl.getUniformLocation(gShader.program, "uSpotLightDirectionWorld2");
 
     // Uniforms do FRAGMENT SHADER
     gShader.uCorDifusaoSpot = gl.getUniformLocation(gShader.program, "uCorDifusaoSpot");
@@ -347,9 +358,11 @@ function crieShaders() {
 var gVertexShaderSrc = `#version 300 es
 in  vec3 aPosition;
 in  vec3 aNormal;
-uniform vec3 uSpotLightDirectionWorld; // Direção do spot light (em World Space)
-uniform vec4 uSpotLightPos; // Posição do spot light (em World Space) 
+uniform vec3 uSpotLightDirectionWorld1; // Direção do spot light 1 (em World Space)
+uniform vec4 uSpotLightPos1; // Posição do spot light 1 (em World Space) 
 
+uniform vec3 uSpotLightDirectionWorld2; // Direção do spot light 2 (em World Space)
+uniform vec4 uSpotLightPos2; // Posição do spot light 2 (em World Space) 
 
 uniform mat4 uModel;
 uniform mat4 uView;
@@ -360,8 +373,12 @@ out vec3 vNormal;
 out vec3 vLight;
 out vec3 vView;
 
-out vec3 vSpotLight;      // Vetor em viwespace do aposition até o spot light 
-out vec3 vSpotLightDirectionView; // vetor de direção do spot light em view space
+out vec3 vSpotLight1;      // Vetor em viwespace do aposition até o spot light 1
+out vec3 vSpotLightDirectionView1; // vetor de direção do spot light 1 em view space
+
+
+out vec3 vSpotLight2;      // Vetor em viwespace do aposition até o spot light 2
+out vec3 vSpotLightDirectionView2; // vetor de direção do spot light 2 em view space
 
 void main() {
     mat4 modelView = uView * uModel;
@@ -371,8 +388,12 @@ void main() {
     vLight = (uView * uLuzPos - pos).xyz;
     vView = -(pos.xyz);
 
-    vSpotLight = (uView * uSpotLightPos - pos).xyz; 
-    vSpotLightDirectionView = normalize(mat3(uView) * uSpotLightDirectionWorld); // Direção do spot light em view space
+    vSpotLight1 = (uView * uSpotLightPos1 - pos).xyz; 
+    vSpotLightDirectionView1 = normalize(mat3(uView) * uSpotLightDirectionWorld1); // Direção do spot light 1 em view space
+
+
+    vSpotLight2 = (uView * uSpotLightPos2 - pos).xyz; 
+    vSpotLightDirectionView2 = normalize(mat3(uView) * uSpotLightDirectionWorld2); // Direção do spot light 2 em view space
 }
 `;
 
@@ -387,10 +408,17 @@ uniform vec4 uCorEspecular;
 uniform float uAlfaEsp;
 
 // Spot light in, vetor do aPosition ate a posição do spot light
-in vec3 vSpotLight;  
+in vec3 vSpotLight1;  
 
 // Spot light direction, vetor de direção do spot light em view space
-in vec3 vSpotLightDirectionView;
+in vec3 vSpotLightDirectionView1;
+
+
+// Spot light in, vetor do aPosition ate a posição do spot light
+in vec3 vSpotLight2;  
+
+// Spot light direction, vetor de direção do spot light em view space
+in vec3 vSpotLightDirectionView2;
 
 // Spot light uniforms
 uniform vec4 uCorDifusaoSpot;   // Cor Difusa = cor da lanterna * cor do objeto
@@ -413,25 +441,44 @@ void main() {
     }
     vec4 cor_point_light= difusao + especular;
 
-    //////// Cálculo do spot light///////
-    vec3 spotLightV = normalize(vSpotLight);
-    float angle = dot(-spotLightV, vSpotLightDirectionView);
-    float spotFactor = smoothstep(uOuterCutoff, uInnerCutoff, angle);
+    //////// Cálculo do spot light 1 ////////
+
+    vec3 spotLightV1 = normalize(vSpotLight1);
+    float angle1 = dot(-spotLightV1, vSpotLightDirectionView1);
+    float spotFactor1 = smoothstep(uOuterCutoff, uInnerCutoff, angle1);
 
 
-    vec3 spotHalfV = normalize(spotLightV + viewV);
-    float kd_spot = max(0.0, dot(normalV, spotLightV));
-    vec4 difusaoSpot = kd_spot * uCorDifusaoSpot;
-    float ks_spot = pow(max(0.0, dot(normalV, spotHalfV)), uAlfaEsp);
-    vec4 especularSpot = vec4(0.0);
-    if (kd_spot > 0.0) {
-        especularSpot = ks_spot * uCorEspecularSpot;
+    vec3 spotHalfV1 = normalize(spotLightV1 + viewV);
+    float kd_spot1 = max(0.0, dot(normalV, spotLightV1));
+    vec4 difusaoSpot1 = kd_spot1 * uCorDifusaoSpot;
+    float ks_spot1 = pow(max(0.0, dot(normalV, spotHalfV1)), uAlfaEsp);
+    vec4 especularSpot1 = vec4(0.0);
+    if (kd_spot1 > 0.0) {
+        especularSpot1 = ks_spot1 * uCorEspecularSpot;
     }
-    vec4 spotLightTotal = (difusaoSpot + especularSpot) * spotFactor;
+    vec4 spotLightTotal1 = (difusaoSpot1 + especularSpot1) * spotFactor1;
+
+    ////////// Cálculo do spot light 2 /////////
+
+    vec3 spotLightV2 = normalize(vSpotLight2);
+    float angle2 = dot(-spotLightV2, vSpotLightDirectionView2);
+    float spotFactor2 = smoothstep(uOuterCutoff, uInnerCutoff, angle2);
+
+
+    vec3 spotHalfV2 = normalize(spotLightV2 + viewV);
+    float kd_spot2 = max(0.0, dot(normalV, spotLightV2));
+    vec4 difusaoSpot2 = kd_spot2 * uCorDifusaoSpot;
+    float ks_spot2 = pow(max(0.0, dot(normalV, spotHalfV2)), uAlfaEsp);
+    vec4 especularSpot2 = vec4(0.0);
+    if (kd_spot2 > 0.0) {
+        especularSpot2 = ks_spot2 * uCorEspecularSpot;
+    }
+    vec4 spotLightTotal2 = (difusaoSpot2 + especularSpot2) * spotFactor2;
+
     ////////////////////////////////////
 
-    corSaida = uCorAmbiente + cor_point_light + spotLightTotal;
-    //corSaida = spotLightTotal;
+    corSaida = uCorAmbiente + cor_point_light + spotLightTotal1 + spotLightTotal2;
+    //corSaida = spotLightTotal2;
     corSaida.a = 1.0;
 }
 `;
